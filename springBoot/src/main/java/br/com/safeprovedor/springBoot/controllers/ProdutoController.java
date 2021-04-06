020835fcff67a899858d246903ecdcdc407d2238
thiagoaugust <=>
2021-04-05 21:26:13 -0300

    Criação do formulário de cadastro de produto

--- springBoot/src/main/java/br/com/safeprovedor/springBoot/controllers/ProdutoController.java
+++ springBoot/src/main/java/br/com/safeprovedor/springBoot/controllers/ProdutoController.java
@@ -1,27 +1,42 @@
 package br.com.safeprovedor.springBoot.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.com.safeprovedor.springBoot.model.entities.Produto;
 import br.com.safeprovedor.springBoot.model.repositories.ProdutoRepository;
 
-@RestController
+@Controller
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
-	@PostMapping
-	public Produto novoProduto(@RequestParam String nome) {
-		Produto produto = new Produto(nome);
+	@GetMapping(value="/novo")
+	public String form(){
+		return "/formulario";
+	}
+	
+	@PostMapping(value = "/novo")
+	public String novoProduto(Produto produto) {
 		produtoRepository.save(produto);
-		
-		return produto;
+		return "redirect:/api/produtos/novo";
+	}
+	
+	@GetMapping("/todos")
+	public List<Produto> todosProduto() {
+		List<Produto> produtos = new ArrayList<>();
+		produtos = (List<Produto>) produtoRepository.findAll();
+		return produtos;
 	}
 
 }
