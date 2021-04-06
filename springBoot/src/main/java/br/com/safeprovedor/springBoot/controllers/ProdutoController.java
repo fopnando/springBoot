f663d69f10d85f84aae4ce0c60d05934dced145e
thiagoaugust <=>
2021-04-06 08:02:24 -0300

    Listar Produtos e padronização de Rotas

--- springBoot/src/main/java/br/com/safeprovedor/springBoot/controllers/ProdutoController.java
+++ springBoot/src/main/java/br/com/safeprovedor/springBoot/controllers/ProdutoController.java
@@ -1,42 +1,38 @@
 package br.com.safeprovedor.springBoot.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.safeprovedor.springBoot.model.entities.Produto;
 import br.com.safeprovedor.springBoot.model.repositories.ProdutoRepository;
+import br.com.safeprovedor.springBoot.routes.Rota;
 
 @Controller
-@RequestMapping("/api/produtos")
 public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
-	@GetMapping(value="/novo")
+	@GetMapping(Rota.FORMULARIO)
 	public String form(){
 		return "/formulario";
 	}
 	
-	@PostMapping(value = "/novo")
+	@PostMapping(Rota.CRIAR)
 	public String novoProduto(Produto produto) {
 		produtoRepository.save(produto);
 		return "redirect:/api/produtos/novo";
 	}
 	
-	@GetMapping("/todos")
-	public List<Produto> todosProduto() {
-		List<Produto> produtos = new ArrayList<>();
-		produtos = (List<Produto>) produtoRepository.findAll();
-		return produtos;
+	@GetMapping(Rota.LISTAR)
+	public ModelAndView listarTodos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Produto> produtos = produtoRepository.findAll();
+		mv.addObject("produtos", produtos);
+		return mv;
 	}
-
+	
 }
---
+++ springBoot/src/main/java/br/com/safeprovedor/springBoot/routes/Rota.java
@@ -0,0 +1,9 @@
+package br.com.safeprovedor.springBoot.routes;
+
+public class Rota {
+	
+	public static final String FORMULARIO = "/api/produtos/novo";
+	public static final String CRIAR = "/api/produtos/novo";
+	public static final String LISTAR = "/api/produtos/listarTodos";
+
+}
